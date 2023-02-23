e7884c83097a997a8631906a1024f0b2c94cc32c
schou <pschou@users.noreply.github.com>
2023-02-23 12:51:36 -0500

    add WriteAtBuf

---
+++ writeatbuf.go
@@ -0,0 +1,184 @@
+package memdiskbuf
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"sort"
+)
+
+// A buffer for WriteAt calls.  The practical use for this module is to
+// reconstruct an original file when only specific fragments are available at
+// any given moment.  This WriterAtBuf assumes the payload being written is
+// immutable, meaning the bytes written and then re-written will never change.
+type WriterAtBuf struct {
+	fh             io.WriterAt
+	written, block int64
+
+	buf   []byte
+	bufSt int64
+	inbuf []startStop
+}
+
+type startStop struct {
+	start, stop int64
+}
+
+// NewWriterAtBuf creates a new WriterAtBuf for buffering WriteAt calls with a
+// specified bufSize.  For optimal performance, the BufSize should be at least
+// 8k and preferably around 32k.
+func NewWriterAtBuf(fh io.WriterAt, bufSize int) *WriterAtBuf {
+	chunks := 16
+	return &WriterAtBuf{
+		fh:    fh,
+		buf:   make([]byte, bufSize),
+		inbuf: make([]startStop, chunks),
+		block: 4 << 10,
+	}
+}
+
+// NewWriterAtBufWithBlockSize creates a new WriterAtBuf for buffering WriteAt
+// calls with a specified bufSize.  For optimal performance, the BufSize should
+// be at least 8k and preferably around 32k.  The block size should be a 2^n
+// multiple of 4k, like 4k, 8k, ... to ensure the best performance for sector
+// size matching.
+func NewWriterAtBufWithBlockSize(fh io.WriterAt, bufSize, blockSize int) *WriterAtBuf {
+	chunks := 16
+	if bufSize < blockSize*2 {
+		bufSize = blockSize * 2
+	}
+	return &WriterAtBuf{
+		fh:    fh,
+		buf:   make([]byte, bufSize),
+		inbuf: make([]startStop, chunks),
+		block: int64(blockSize),
+	}
+}
+
+// WriteAt writes len(b) bytes to the File starting at byte offset off. It
+// returns the number of bytes written and an error, if any. WriteAt returns a
+// non-nil error when n != len(b).
+func (w *WriterAtBuf) WriteAt(p []byte, off int64) (n int, err error) {
+	offEnd, bufEnd := off+int64(len(p)), w.bufSt+int64(len(w.buf))
+	if off < w.bufSt { // We've moved past this point
+		if offEnd <= w.bufSt { // no-op
+			return len(p), nil
+		}
+		// We can pick up a bit of what's left for the buffer, it should not come
+		// to this point if the data is not overlapping, but account for some
+		// randomness
+		n = copy(w.buf[w.bufSt-off:], p)
+		add(&w.inbuf, w.bufSt, w.bufSt+int64(n))
+		n += int(w.bufSt - off)
+		err = w.shift()
+		return
+	}
+	if offEnd > bufEnd {
+		// Miss, trigger an error
+		return 0, fmt.Errorf("WriteAt outside buffer window %d-%d > [%d-%d]", off, offEnd, w.bufSt, bufEnd)
+	}
+	// Buffer hit!
+	add(&w.inbuf, off, offEnd)
+	n = copy(w.buf[off-w.bufSt:], p)
+	err = w.shift()
+	return
+}
+
+func (w *WriterAtBuf) shift() (err error) {
+	var n int
+	for chunkEnd := w.bufSt + w.block; w.inbuf[0].start == w.bufSt && w.inbuf[0].stop >= chunkEnd; chunkEnd = w.bufSt + w.block {
+		// Good times, go ahead and write our 4k
+		if n, err = w.fh.WriteAt(w.buf[:w.block], w.inbuf[0].start); err != nil {
+			return // Something bad happened
+		} else if n != int(w.block) {
+			return fmt.Errorf("Could not write %d, instead wrote %d", w.block, n)
+		}
+		w.bufSt, w.inbuf[0].start, w.written = chunkEnd, chunkEnd, chunkEnd+int64(n)
+		for i, j := 0, int(w.block); j < len(w.buf); i, j = i+1, j+1 {
+			w.buf[i] = w.buf[j]
+		}
+	}
+	return
+}
+
+// Total amount written to disk
+func (w *WriterAtBuf) Written() int64 {
+	return w.written
+}
+
+// Flush what is in the buffer to disk, but don't advance the buffer to prevent
+// blocks getting out of sync.  An error will be returned if there are gaps in
+// the buffer.
+func (w *WriterAtBuf) Flush() (err error) {
+	var n int
+	condense(w.inbuf)
+	if w.inbuf[0].start == w.bufSt {
+		toWrite := w.inbuf[0].stop - w.inbuf[0].start
+		if n, err = w.fh.WriteAt(w.buf[:toWrite], w.inbuf[0].start); err != nil {
+			return // Something bad happened
+		} else if n != int(toWrite) {
+			return fmt.Errorf("Could not write %d, instead wrote %d", w.block, n)
+		}
+		w.written = w.bufSt + int64(n)
+	}
+	for _, b := range w.inbuf {
+		if b.stop > w.written {
+			return errors.New("Could not flush, missing one or more segments")
+		}
+	}
+	return nil
+}
+
+func add(set *[]startStop, start, stop int64) {
+	recPos := -1 // Mark any open position for assignment
+	seen := *set
+	for i := range seen {
+		if seen[i].stop == 0 {
+			recPos = i
+		}
+		if seen[i].stop >= start && stop >= seen[i].start { // Append to an existing block
+			seen[i].start, seen[i].stop = min(seen[i].start, start), max(seen[i].stop, stop)
+			return
+		}
+	}
+	if recPos == -1 {
+		seen = append(seen, startStop{start: start, stop: stop})
+		*set = seen
+	} else {
+		seen[recPos].start, seen[recPos].stop = start, stop
+	}
+	condense(*set)
+}
+
+func condense(seen []startStop) {
+	// Sort the values to ensure the smallest is at the front
+	sort.Slice(seen, func(i, j int) bool {
+		if seen[i].stop == 0 && seen[j].stop > 0 { // send zeros to the end
+			return false
+		}
+		return seen[i].start < seen[j].start
+	})
+
+	var i int
+	var workDone bool
+	for i < len(seen) {
+		if seen[i].stop == 0 {
+			i++
+			continue
+		}
+		workDone = false
+		for j := i + 1; j < len(seen); j++ {
+			if seen[j].stop == 0 {
+				continue
+			}
+			if seen[i].stop+1 >= seen[j].start && seen[j].stop+1 >= seen[i].start {
+				seen[i].start, seen[i].stop, seen[j].start, seen[j].stop =
+					min(seen[i].start, seen[j].start), max(seen[i].stop, seen[j].stop), 0, 0
+				workDone = true
+			}
+		}
+		if !workDone {
+			i, workDone = i+1, false
+		}
+	}
+}
---
+++ writeatbuf_test.go
@@ -0,0 +1,40 @@
+package memdiskbuf_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pschou/go-memdiskbuf"
+)
+
+func ExampleNewWriterAtBufWithBlockSize() {
+	fh, err := os.OpenFile("example_file", os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fh.Truncate(100)
+
+	wab := memdiskbuf.NewWriterAtBufWithBlockSize(fh, 30, 10)
+	wab.WriteAt([]byte("world,"), 6)
+	wab.WriteAt([]byte("hello "), 0)
+	wab.WriteAt([]byte("you be"), 18)
+	wab.WriteAt([]byte(" will "), 12)
+	wab.WriteAt([]byte(" my fr"), 24)
+	wab.WriteAt([]byte("iend.\n"), 30)
+	err = wab.Flush()
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+
+	dat := make([]byte, 36)
+	fh.ReadAt(dat, 0)
+	fmt.Printf("%q\n", dat)
+	fmt.Println("written:", wab.Written())
+
+	fh.Close()
+	os.Remove("example_file")
+	// Output:
+	// "hello world, will you be my friend.\n"
+	// written: 36
+}
