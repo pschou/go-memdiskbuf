49fa1cc436e134d997a9e04446abfe314f59c35d
schou <pschou@users.noreply.github.com>
2023-02-23 20:38:15 -0500

    add Reset and StreamFunc for handling in-ordered-data in memory

--- writeatbuf.go
+++ writeatbuf.go
@@ -9,11 +9,22 @@ import (
 
 // A buffer for WriteAt calls.  The practical use for this module is to
 // reconstruct an original file when only specific fragments are available at
-// any given moment.  This WriterAtBuf assumes the payload being written is
-// immutable, meaning the bytes written and then re-written will never change.
+// any given moment, such as from a semi-ordered UDP stream.  Note: A larger
+// buffer size will prevent buffer overruns when UDP traffic has more sever
+// unordering in packets.
+//
+// This WriterAtBuf
+// assumes the payload being written is immutable, meaning the bytes written
+// and then re-written will never change.
 type WriterAtBuf struct {
 	fh             io.WriterAt
 	written, block int64
+
+	// User provided function for in-memory inspection of the data before it is
+	// written to disk.  A useful way to use this is for calling a Hash function
+	// over the data so it doesn't have to be re-read from the disk.
+	StreamFunc  func([]byte)
+	streamFlush int
 
 	buf   []byte
 	bufSt int64
@@ -24,15 +35,24 @@ type startStop struct {
 	start, stop int64
 }
 
+// Reset will reset the counters and reuse the underlying buffer for another
+// WriterAt interface.
+func (w *WriterAtBuf) Reset(fh io.WriterAt) {
+	w.fh = fh
+	w.written = 0
+	w.StreamFunc, w.streamFlush = nil, 0
+	w.bufSt = 0
+	w.inbuf = make([]startStop, 3)
+}
+
 // NewWriterAtBuf creates a new WriterAtBuf for buffering WriteAt calls with a
 // specified bufSize.  For optimal performance, the BufSize should be at least
 // 8k and preferably around 32k.
 func NewWriterAtBuf(fh io.WriterAt, bufSize int) *WriterAtBuf {
-	chunks := 16
 	return &WriterAtBuf{
 		fh:    fh,
 		buf:   make([]byte, bufSize),
-		inbuf: make([]startStop, chunks),
+		inbuf: make([]startStop, 3),
 		block: 4 << 10,
 	}
 }
@@ -42,15 +62,17 @@ func NewWriterAtBuf(fh io.WriterAt, bufSize int) *WriterAtBuf {
 // be at least 8k and preferably around 32k.  The block size should be a 2^n
 // multiple of 4k, like 4k, 8k, ... to ensure the best performance for sector
 // size matching.
+//
+// The bufSize must be greater than twice the blockSize to ensure that the
+// block can properly be blocked off and written to the destination.
 func NewWriterAtBufWithBlockSize(fh io.WriterAt, bufSize, blockSize int) *WriterAtBuf {
-	chunks := 16
 	if bufSize < blockSize*2 {
 		bufSize = blockSize * 2
 	}
 	return &WriterAtBuf{
 		fh:    fh,
 		buf:   make([]byte, bufSize),
-		inbuf: make([]startStop, chunks),
+		inbuf: make([]startStop, 3),
 		block: int64(blockSize),
 	}
 }
@@ -65,8 +87,8 @@ func (w *WriterAtBuf) WriteAt(p []byte, off int64) (n int, err error) {
 			return len(p), nil
 		}
 		// We can pick up a bit of what's left for the buffer, it should not come
-		// to this point if the data is not overlapping, but account for some
-		// randomness
+		// to this point if the data is not overlapping, but this method account
+		// for some randomness
 		n = copy(w.buf[w.bufSt-off:], p)
 		add(&w.inbuf, w.bufSt, w.bufSt+int64(n))
 		n += int(w.bufSt - off)
@@ -87,11 +109,17 @@ func (w *WriterAtBuf) WriteAt(p []byte, off int64) (n int, err error) {
 func (w *WriterAtBuf) shift() (err error) {
 	var n int
 	for chunkEnd := w.bufSt + w.block; w.inbuf[0].start == w.bufSt && w.inbuf[0].stop >= chunkEnd; chunkEnd = w.bufSt + w.block {
+		// We have data!  Call the stream function in case we have one
+		if w.StreamFunc != nil {
+			w.StreamFunc(w.buf[w.streamFlush:w.block])
+			w.streamFlush = 0
+		}
+
 		// Good times, go ahead and write our block
 		if n, err = w.fh.WriteAt(w.buf[:w.block], w.inbuf[0].start); err != nil {
 			return // Something bad happened
 		} else if n != int(w.block) {
-			return fmt.Errorf("Could not write %d, instead wrote %d", w.block, n)
+			return fmt.Errorf("Could not write %d, instead wrote %d, check the Block size used.", w.block, n)
 		}
 		w.bufSt, w.inbuf[0].start, w.written = chunkEnd, chunkEnd, chunkEnd+int64(n)
 		for i, j := 0, int(w.block); j < len(w.buf); i, j = i+1, j+1 {
@@ -108,7 +136,7 @@ func (w *WriterAtBuf) Written() int64 {
 
 // FlushAll - flushes all the buffer to disk, gaps and all.  Doesn't advance
 // the buffer to prevent blocks getting out of sync.  An error will be returned
-// if there are gaps in the buffer.
+// if there was an error writing to the target file.
 func (w *WriterAtBuf) FlushAll() (err error) {
 	var n int
 	for _, inbuf := range w.inbuf {
@@ -131,11 +159,20 @@ func (w *WriterAtBuf) Flush() (err error) {
 	var n int
 	if w.inbuf[0].start == w.bufSt {
 		toWrite := w.inbuf[0].stop - w.inbuf[0].start
+
+		// We have data!  Call the stream function in case we have one
+		if w.StreamFunc != nil {
+			w.StreamFunc(w.buf[w.streamFlush:toWrite])
+			w.streamFlush = int(toWrite)
+		}
+
 		if n, err = w.fh.WriteAt(w.buf[:toWrite], w.inbuf[0].start); err != nil {
 			return // Something bad happened
 		} else if n != int(toWrite) {
 			return fmt.Errorf("Could not write %d, instead wrote %d", w.block, n)
 		}
+
+		// Update counters
 		w.written = w.bufSt + int64(n)
 	}
 	for _, b := range w.inbuf {
@@ -160,10 +197,11 @@ func (w *WriterAtBuf) Flushable() (n int64, err error) {
 	return
 }
 
+// Add will add a fragment to a start-stop list for defragmenting.
 func add(set *[]startStop, start, stop int64) {
 	recPos := -1 // Mark any open position for assignment
 	seen := *set
-	defer condense(*set)
+	defer condense(*set) // Upon add finishing, make sure the resultant list is condensed
 	for i := range seen {
 		if seen[i].stop == 0 && recPos == -1 {
 			recPos = i
@@ -181,6 +219,8 @@ func add(set *[]startStop, start, stop int64) {
 	}
 }
 
+// Bring all the fragments together, defragment them, and build larger blocks
+// with 0 being the first.
 func condense(seen []startStop) {
 	// Sort the values to ensure the smallest is at the front
 	sort.Slice(seen, func(i, j int) bool {
